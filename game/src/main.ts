6165cf096089a8cbbc4c14e201ed988e43d2b2c3
Adam Vlček <vlcek-adam@seznam.cz>
2025-05-20 17:59:52 +0200

    moved rendering logic to player object - mesmerize

--- game/src/main.ts
+++ game/src/main.ts
@@ -4,14 +4,16 @@ import { Player } from "./objects/player.js";
 document.addEventListener("DOMContentLoaded", startGame);
 
 function startGame() {
-    const canvas = document.getElementById("test") as HTMLCanvasElement;
+    const canvasF = document.getElementById("foreground") as HTMLCanvasElement;
+    const canvasB = document.getElementById("background") as HTMLCanvasElement;
 
-    if (canvas) {
-        const ctx = canvas.getContext("2d");
-        if (ctx === null) {
+    if (canvasF && canvasB) {
+        const ctxF = canvasF.getContext("2d");
+        const ctxB = canvasB.getContext("2d");
+        if (ctxF === null || ctxB === null) {
             console.log("Failed to get canvas context");
         } else {
-            var game = new Game(ctx);
+            var game = new Game(ctxF as CanvasRenderingContext2D, ctxB as CanvasRenderingContext2D);
         }
     } else {
         document.body.innerHTML = "Error: failed to get canvas for game";
@@ -23,22 +25,26 @@ function startGame() {
 }
 
 class Game {
-    private ctx: CanvasRenderingContext2D;
+    private ctxF: CanvasRenderingContext2D;
+    private ctxB: CanvasRenderingContext2D;
     private buffer: CanvasRenderingContext2D;
     private lastUpdate = performance.now();
     private accumulator = 0;
-    private readonly timestep = 1000 / 60;
+    private readonly fps = 60;
+    private readonly timestep = 1000 / this.fps;
 
-    private Player: Player;
+    private Players: Player[] = [];
     private Controlls: Controlls;
 
-    constructor(ctx: CanvasRenderingContext2D) {
-        this.ctx = ctx;
+    constructor(ctxF: CanvasRenderingContext2D, ctxB: CanvasRenderingContext2D) {
+        this.ctxF = ctxF;
+        this.ctxB = ctxB;
         const buffer = document.createElement("canvas") as HTMLCanvasElement;
         buffer.width = 800;
         buffer.height = 600;
         this.buffer = buffer.getContext("2d") as CanvasRenderingContext2D;
-        this.Player = new Player("1", "Player 1", "red", 200, 200, 0);
+        this.Players[0] = new Player("1", "Player 1", "red", 200, 200, 0);
+        this.Players[1] = new Player("1s", "Player 12", "red", 100, 100, 180);
         this.Controlls = new Controlls();
 
         requestAnimationFrame(this.loop.bind(this));
@@ -59,43 +65,19 @@ class Game {
     }
 
     private update() {
-        var speed = 8;
-        if (this.Controlls.up) {
-            this.Player.y -= Math.cos((this.Player.rotation / 180) * Math.PI) * speed;
-            this.Player.x += Math.sin((this.Player.rotation / 180) * Math.PI) * speed;
-        }
-        if (this.Controlls.down) {
-            this.Player.y += Math.cos((this.Player.rotation / 180) * Math.PI) * speed;
-            this.Player.x -= Math.sin((this.Player.rotation / 180) * Math.PI) * speed;
-        }
-        if (this.Controlls.left) {
-            this.Player.rotation -= speed;
-        }
-        if (this.Controlls.right) {
-            this.Player.rotation += speed;
-        }
+        // update players
+        this.Players.forEach((player) => {
+            player.update();
+        });
     }
 
     private render() {
-        var img = new Image();
-        img.src = "player.png";
-
-        var relX = this.Player.x + img.width / 2;
-        var relY = this.Player.y + img.height / 2;
-
-        this.buffer.reset();
-
-        this.buffer.fillStyle = "#E4DFDA";
-
-        this.buffer.fillRect(0, 0, 800, 600);
-        this.buffer.translate(relX, relY);
-        this.buffer.rotate((this.Player.rotation * Math.PI) / 180);
-        this.buffer.translate(-relX, -relY);
-
-        this.buffer.drawImage(img, this.Player.x, this.Player.y);
-        // this.buffer.fillRect(this.Player.x, this.Player.y, this.Player.width, this.Player.height);
-
-        this.ctx.reset();
-        this.ctx.drawImage(this.buffer.canvas, 0, 0);
+        // render foreground
+        // render players
+        this.Players.forEach((player) => {
+            player.render(this.ctxF);
+        });
+        //this.ctxF.reset();
+        //this.ctxF.drawImage(this.buffer.canvas, 0, 0);
     }
 }
--- game/src/objects/player.ts
+++ game/src/objects/player.ts
@@ -1,3 +1,5 @@
+import { Controlls } from "../controlls.js";
+
 export class Player {
     public id: string;
     public name: string;
@@ -7,6 +9,9 @@ export class Player {
     public rotation: number;
     public width: number = 50;
     public height: number = 50;
+    public isAlive: boolean = true;
+
+    private Controlls: Controlls;
 
     constructor(id: string, name: string, color: string, x: number, y: number, rotation: number) {
         this.id = id;
@@ -15,5 +20,54 @@ export class Player {
         this.x = x;
         this.y = y;
         this.rotation = rotation;
+
+        this.Controlls = new Controlls();
+    }
+
+    public set(id: string, name: string, color: string, x: number, y: number, rotation: number) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.rotation = rotation;
+    }
+
+    public update() {
+        var speed = 4;
+        var rotSpeed = 5.5;
+        if (this.Controlls.up) {
+            this.y -= Math.cos((this.rotation / 180) * Math.PI) * speed;
+            this.x += Math.sin((this.rotation / 180) * Math.PI) * speed;
+        }
+        if (this.Controlls.down) {
+            this.y += Math.cos((this.rotation / 180) * Math.PI) * speed;
+            this.x -= Math.sin((this.rotation / 180) * Math.PI) * speed;
+        }
+        if (this.Controlls.left) {
+            this.rotation -= rotSpeed;
+        }
+        if (this.Controlls.right) {
+            this.rotation += rotSpeed;
+        }
+    }
+
+    public render(buffer: CanvasRenderingContext2D) {
+        var img = new Image();
+
+        img.src = "player.png";
+
+        var relX = this.x + img.width / 2;
+        var relY = this.y + img.height / 2;
+
+        buffer.fillStyle = "#E4DFDA";
+        buffer.fillRect(0, 0, 800, 600);
+        buffer.translate(relX, relY);
+        buffer.rotate((this.rotation * Math.PI) / 180);
+        buffer.translate(-relX, -relY);
+        buffer.drawImage(img, this.x, this.y);
+        buffer.save();
+
+        //buffer.fillRect(this.x, this.y, this.width, this.height);
     }
 }
